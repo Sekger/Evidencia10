170c1ca3264001d81d9a73392f38c3d375b5c095
benja <b.silva08@ufromail.cl>
2023-06-05 00:57:36 -0400

    primer caso

---
+++ src/main/java/org/example/Mascota.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Mascota extends Animal {
+
+	Collection<Ñiño> dueño;
+	private String nombre;
+	private String color;
+	private String caracter;
+
+	private void ladrar() {
+		// TODO - implement Mascota.ladrar
+		throw new UnsupportedOperationException();
+	}
+
+	private void comer() {
+		// TODO - implement Mascota.comer
+		throw new UnsupportedOperationException();
+	}
+
+	private void morder() {
+		// TODO - implement Mascota.morder
+		throw new UnsupportedOperationException();
+	}
+
+	private void rasguñar() {
+		// TODO - implement Mascota.rasguñar
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/main/java/org/example/Ñiño.java
@@ -0,0 +1,8 @@
+public class Ñiño extends Estudiante {
+
+	Mascota mascota;
+	private String nombre;
+	private int edad;
+	private String sexo;
+
+}
